3ee72864e987df3d97929fa278dc69dd56181d50
Kenzo Nirasawa <kemuzo.nira@gmail.com>
2016-12-15 02:05:20 +0900

    first commit

--- TindableViewSample/AppDelegate.swift
+++ TindableViewSample/AppDelegate.swift
@@ -15,7 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        let mainVC = ViewController()
+        let navVC = UINavigationController(rootViewController: mainVC)
+        self.window?.rootViewController = navVC
+        self.window?.makeKeyAndVisible()
         return true
     }
 
